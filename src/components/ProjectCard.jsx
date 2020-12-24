527c8cc8ce1b874034e38a95c5c1a6fe86f32968
Daniel John Keefer <xxkeefer.code@gmail.com>
2020-12-24 11:43:16 +1000

    Update ProjectCard.jsx
    adjusted grid item width on xl size screens

--- src/components/ProjectCard.jsx
+++ src/components/ProjectCard.jsx
@@ -53,7 +53,7 @@ const ProjectCard = ({
   const classes = useStyles();
   const [expanded, setExpanded] = useState(false);
   return (
-    <Grid item xs={12} sm={6} md={6} lg={4} xl={3}>
+    <Grid item xs={12} sm={6} md={6} lg={4} xl={4}>
       <Card className={classes.card}>
         <CardContent>
           <Typography className={classes.project} variant="h5">
